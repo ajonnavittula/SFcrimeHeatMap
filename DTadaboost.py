92b9d56142ef1f68e7482a230a26c131311cb33b
AnanyDwivedi <adwivedi@wpi.edu>
2016-11-21 03:01:56 -0500

    Added Dataset with added features. Added DecisionTree with Adaboost code

---
+++ DTadaboost.py
@@ -0,0 +1,90 @@
+from sklearn.externals.six.moves import zip
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+from sklearn.datasets import make_gaussian_quantiles
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.decomposition import PCA
+
+import pandas as pd
+
+data=pd.read_csv("SF_ProcessedEightFeatures.csv",engine='python',delimiter=',', header=0)
+d=data.as_matrix(columns=None)
+
+#X, y = make_gaussian_quantiles(n_samples=13000, n_features=10,
+#                               n_classes=3, random_state=1)
+
+#n_split = 3000
+
+#X_train, X_test = X[:n_split], X[n_split:]
+#y_train, y_test = y[:n_split], y[n_split:]
+
+temp = np.asarray(d)
+#print temp.dtype
+np.random.shuffle(temp)
+temp2 = temp
+
+pca = PCA(n_components=8)
+pca.fit(temp[:,0:8])
+#print pca.explained_variance_ratio_
+# print pca.components_
+X_train_n=temp2[0:1048575,0:8]
+y_train_n=temp2[0:1048575,8]
+X_train = temp2[0:629145,0:8]
+X_train = np.array(X_train).astype(np.float)
+#print X_train.dtype
+print ".........................."
+y_train = temp2[0:629145,8]
+y_train = np.array(y_train).astype(int)
+#print(y_train)
+#print y_train.dtype
+X_val = temp2[629145:838860,0:8]
+X_val = np.array(X_val).astype(np.float)
+
+y_val = temp2[629145:838860,8]
+y_val = np.array(y_val).astype(np.int)
+#print y_val.dtype
+X_test = temp2[838860:1048575,0:8]
+X_test = np.array(X_test).astype(np.float)
+
+y_test = temp2[838860:1048575,8]
+y_test = np.array(y_test).astype(np.float)
+y_test = np.array(y_test).astype(np.int)
+
+print y_train
+bdt_discrete = AdaBoostClassifier(
+    DecisionTreeClassifier(max_depth=2),
+    n_estimators=600,
+    learning_rate=1.5,
+    algorithm="SAMME")
+
+bdt_discrete.fit(X_train, y_train)
+
+
+##########################################################################################
+#clf2.fit(Xtr, y_train)
+y_train_pred = bdt_discrete.predict(X_train)
+print "Training Complete..."
+print "Testing..."
+accutr = np.mean(y_train == y_train_pred)                         
+y_test_pred = bdt_discrete.predict(X_test)
+accut = np.mean(y_test == y_test_pred)
+
+print "Train Accuracy = %f Test Accuracy = %f "%(accutr,accut)
+
+y_train_pred = y_train_pred.T
+print "Testing..."
+accutr = np.mean(y_train == y_train_pred)                         
+
+y_test_pred = y_test_pred.T
+accut = np.mean(y_test == y_test_pred)
+
+print "Train Accuracy = %f Test Accuracy = %f "%(accutr,accut)
+
+################################################################################################
+
+
